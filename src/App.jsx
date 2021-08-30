0aaf38086c6e844710f03ce6503a65e4ca4f067a
shinhonggyu <zowoz0@naver.com>
2021-08-30 21:25:52 +0900

    finish desktop-v

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,77 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Contact from './components/Contact';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Navbar from './components/Navbar';
+import Price from './components/Price';
+import Service from './components/Service';
+import './app.css';
 
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
+  position: relative;
 `;
 
-const IntroShape = styled.div`
+const Shape = css`
   width: 100%;
   height: 100%;
   position: absolute;
   top: 0;
   left: 0;
+  z-index: -1;
+`;
+
+const IntroShape = styled.div`
+  ${Shape}
   clip-path: polygon(67% 0%, 100% 0%, 100% 100%, 55% 100%);
   background-color: crimson;
-  z-index: -1;
+`;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0% 0%, 55% 0%, 33% 100%, 0% 100%);
+  background-color: pink;
+`;
+
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0% 0%, 33% 0%, 33% 100%, 0% 100%);
+  background-color: #f88497;
+`;
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0%, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
 `;
 
 function App() {
   return (
-    <Container>
-      <Navbar />
-      <Intro />
-      <IntroShape />
-    </Container>
+    <div className="app">
+      <Container>
+        <Navbar />
+        <Intro />
+        <IntroShape />
+      </Container>
+      <Container>
+        <Feature />
+        <FeatureShape />
+      </Container>
+      <Container>
+        <Service />
+        <ServiceShape />
+      </Container>
+      <Container>
+        <Price />
+        <PriceShape />
+      </Container>
+      <Container>
+        <Contact />
+        <Footer />
+      </Container>
+    </div>
   );
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,137 @@
+import styled from 'styled-components';
+import Map from '../img/map.png';
+import Phone from '../img/phoneSm.png';
+import Send from '../img/send.png';
+
+const Container = styled.div`
+  height: 90%;
+  background: url('https://www.toptal.com/designers/subtlepatterns/patterns/double-bubble-outline.png');
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormContainer = styled.div`
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  margin: 50px;
+  margin-top: 0;
+  margin-left: 119px;
+`;
+
+const Form = styled.form`
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LeftForm = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+`;
+
+const RightForm = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /* width: 50%; */
+`;
+
+const Input = styled.input`
+  width: 200px;
+  padding: 20px;
+`;
+
+const TextArea = styled.textarea`
+  width: 200px;
+  height: 60%;
+  padding: 20px;
+`;
+
+const Button = styled.button`
+  border: none;
+  padding: 15px;
+  background-color: darkblue;
+  color: white;
+  font-size: 20px;
+  border-radius: 10px;
+  margin-top: 20px;
+  cursor: pointer;
+`;
+
+const AddressContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AddressItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 50px;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  margin-right: 20px;
+`;
+const Text = styled.span`
+  font-size: 20px;
+  margin-right: 15px;
+`;
+
+const Contact = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <FormContainer>
+          <Title>
+            Questions? <br /> Let's Get In Touch
+          </Title>
+          <Form>
+            <LeftForm>
+              <Input placeholder="Your Name" />
+              <Input placeholder="Your Email" />
+              <Input placeholder="Subject" />
+            </LeftForm>
+            <RightForm>
+              <TextArea placeholder="Your Message" />
+              <Button>send</Button>
+            </RightForm>
+          </Form>
+        </FormContainer>
+        <AddressContainer>
+          <AddressItem>
+            <Icon src={Map} />
+            <Text>123 Park Avenue St., New York, USA</Text>
+          </AddressItem>
+          <AddressItem>
+            <Icon src={Phone} />
+            <Text>+1 631 1234 5678</Text>
+            <Text>+1 326 1234 5678</Text>
+          </AddressItem>
+          <AddressItem>
+            <Icon src={Send} />
+            <Text>contact@lama.dev</Text>
+            <Text>sales@lama.dev</Text>
+          </AddressItem>
+        </AddressContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Contact;
